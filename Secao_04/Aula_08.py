7207852ee6810372c99851d898e98fd2de22aef2
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 21:37:32 -0300

    Metodos para Strings

---
+++ Secao_04/Aula_08.py
@@ -0,0 +1,10 @@
+# Metodos para Strings - Aula 8
+
+mensagem = 'gosto de comida caseira'
+#index      0123456789
+print(mensagem.lower())
+print(mensagem.upper())
+print(mensagem.capitalize())
+print(mensagem.find('osto'))
+print(mensagem.replace('gosto de', 'amo'))
+print(mensagem.strip())
