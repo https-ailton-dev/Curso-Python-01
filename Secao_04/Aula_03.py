90acfbded334dfbad5673aef3724dadf3d8f39fa
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 21:29:26 -0300

    Praticando com Strings e Integers

---
+++ Secao_04/Aula_03.py
@@ -0,0 +1,9 @@
+# Praticando com Strings e Integers - Aula 3
+# O André tem 32 anos de idade e mora na cidade de São Paulo.
+
+nome = 'André'
+idade = 32
+cidade = 'São Paulo'
+
+print('O ' + nome + ' tem ' + str(idade) + ' anos de idade e mora em ' +
+      cidade + '.')
