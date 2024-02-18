4895534ba8a400d700c33e7aea72943a80aaae1f
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 21:56:12 -0300

    Multiplos Operadores de Comparacao

---
+++ Secao_06/Aula_03.py
@@ -0,0 +1,17 @@
+#Multiplos Operadores de Comparação - Aula 3
+
+valor = 30
+
+if 20<=valor<40:
+    print('Produto aceito')
+
+else:
+    print('Produto não aceito')
+
+print('Fim')
+
+if valor >= 20 and valor < 40:
+    print('Produto aceito')
+
+else:
+    print('Produto não aceito')
