1afd74adcac803cc24c9b7cb3329667ed45e6ad9
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 21:42:04 -0300

    Operadores Matematicos em Python

---
+++ Secao_05/Aula_01.py
@@ -0,0 +1,13 @@
+#Operadores Matematicos em Python - Aula 1
+
+# + (Adição ou sinal positivo)
+# - (Subtração ou sinal negativo)
+# * (Multiplicação)
+# / (Divisão)
+# // (Divisão inteira)
+# % (Módulo) 
+# ** (Exponenciação)
+
+calculo = 2 + 2 ** 3 
+
+print(calculo)
