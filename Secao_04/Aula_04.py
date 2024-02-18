d560900e3f9bc1b90778216cdd2f00bfd66e45e8
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 21:31:47 -0300

    Adicionando Input

---
+++ Secao_04/Aula_04.py
@@ -0,0 +1,8 @@
+# Adicionando Input - Aula 4
+
+nome = input('Qual é o seu nome: ')
+idade = input('Qual é a sua idade: ')
+cidade = input('Onde você mora: ')
+
+print('O ' + nome + ' tem ' + str(idade) + ' anos de idade e mora em ' +
+      cidade + '.')
