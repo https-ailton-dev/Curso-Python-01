00cc65d166b5da468062b8a6ed6610df8bf83090
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 21:44:01 -0300

    Operadores de Comparacao

---
+++ Secao_05/Aula_02.py
@@ -0,0 +1,12 @@
+#Operadores de Comparação - Aula 2
+
+# >(Maior que) 	
+# <(Menor que) 	
+# == (Igual a) 	
+# != (Diferente de) 	
+# >= (Maior ou igual a) 	
+# <= (Menor ou igual a)
+
+operadores = 'Banana' > 'Bananaa'
+
+print(operadores)
