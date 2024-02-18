9cc14e4a54d898bfcc9a18cd7589c5341dc85839
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 21:53:37 -0300

    Operadores Logicos

---
+++ Secao_06/Aula_02.py
@@ -0,0 +1,10 @@
+#Operadores Lógicos - Aula 2
+
+renda_acima_5mil = True
+nome_limpo = False
+
+if renda_acima_5mil and nome_limpo:
+    print('Financiamento Aprovado')
+
+else:
+    print('Financiamento Negado')
