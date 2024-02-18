b05ff13ebec90e06cffd4fff4b0d3e38196f1686
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 22:03:42 -0300

    For Loop | Nested Loops

---
+++ Secao_06/Aula_07.py
@@ -0,0 +1,11 @@
+#For Loop | Nested Loops - Aula 7
+
+#Outer loop
+#Inner Loop
+
+#Outer loop
+for numero1 in range(1, 6):
+    print('Produto ' + str(numero1))
+    #Inner Loop
+    for numero2 in range(5):
+        print(numero1, numero2)
