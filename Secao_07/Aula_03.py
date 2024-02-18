befd4e667e146306022a4ce9a07e5e8253a64f43
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 22:22:31 -0300

    Criando uma funcao de soma

---
+++ Secao_07/Aula_03.py
@@ -0,0 +1,10 @@
+#Criando uma função de soma - Aula 03
+
+def somar_dois_numeros():
+    numero1 = 10
+    numero2 = 5
+    resultado = numero1 + numero2
+    print(resultado)
+
+
+somar_dois_numeros()
