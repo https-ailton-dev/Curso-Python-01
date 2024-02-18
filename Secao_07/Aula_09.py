a33bb348c3f00ebe4b5e291d2a243e859bf4d7ee
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 22:31:55 -0300

    Importando um Modulo

---
+++ Secao_07/Aula_09.py
@@ -0,0 +1,18 @@
+#Importando um Módulo - Aula 09
+
+#qual é o numero fatorial de 4 == 4*3*2*1 = 24
+
+
+#utilizando módulo
+import math
+
+print(math.factorial(4))
+
+
+#manual
+fatorial = 4 * 3 * 2 * 1
+
+print(fatorial)
+
+
+print(math.ceil(2.7))
