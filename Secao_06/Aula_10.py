7b989a40074c916e26a984cb887d75963837d3ce
Ailton Daniel <ailton.developer@gmail.com>
2024-02-17 22:10:27 -0300

    Conhecendo While Loop

---
+++ Secao_06/Aula_10.py
@@ -0,0 +1,10 @@
+#Conhecendo While Loop - Aula 10
+#Excelentes paea loops dependentes de uma condição
+#Criar uma promoção para um produto no valor de R$100.
+
+valor = 100
+dia = 0
+while valor > 20:
+    dia +=1
+    print(f'No dia {dia} o produto ser vendido por R${valor}')
+    valor-=5
