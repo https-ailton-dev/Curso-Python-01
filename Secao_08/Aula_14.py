7f5883f8941897b6835d5a9164df31e79fd06b84
Ailton Daniel <ailton.developer@gmail.com>
2024-02-19 20:35:08 -0300

    Sets com strings

---
+++ Secao_08/Aula_14.py
@@ -0,0 +1,13 @@
+#Sets com strings
+
+    #Similar a listas
+    #Evita itens duplicados
+    #Não utiliza index
+
+set1 = {'a', 'b', 'c'}
+set2 = {'a', 'd', 'e'}
+set3 = {'c', 'd', 'f'}
+
+set4 = set1.difference(set3)
+
+print(set4)
