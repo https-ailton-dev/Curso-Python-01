360ee0fea287050771dab07cbcd1db855245b531
Ailton Daniel <ailton.developer@gmail.com>
2024-02-19 19:30:45 -0300

    Manipulando Listas

---
+++ Aula_02.py
@@ -0,0 +1,11 @@
+########################################################
+#Manipulando Listas - Aula 02
+
+cidades = ['Rio de Janeiro', 'São Paulo', 'Salvador', 'Belo Horizonte']
+#                0                1            2              3
+#               -4               -3           -2             -1
+
+
+cidades[0] = 'Brasilia'
+
+print(cidades)
---
+++ Secao_08/Aula_01.py
@@ -0,0 +1,14 @@
+#Estruturas de Dados - Seção 08
+
+########################################################
+#Introdução a Listas - Aula 01
+#Armazenar mais de uma informação em variáveis.
+#Manter a sequência dos dados em uma varíavel.
+
+cidade1 = 'Rio de Janeiro'
+cidade2 = 'São Paulo'
+cidade3 = 'Salvador'
+
+cidades = ['Rio de Janeiro', 'São Paulo', 'Salvador']
+
+print(cidades)
