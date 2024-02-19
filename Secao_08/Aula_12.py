acd4f5e41a61f57f18045e196a882aa956006696
Ailton Daniel <ailton.developer@gmail.com>
2024-02-19 20:19:42 -0300

    Criando Sets

---
+++ Secao_08/Aula_12.py
@@ -0,0 +1,17 @@
+#Criando Sets
+
+    #Similar a listas
+    #Evita itens duplicados
+    #Não utiliza index
+
+list1 = [10, 20, 30, 40, 50]
+list2 = [10, 20, 60, 70]
+
+num1 = set(list1)
+num2 = set(list2)
+
+print(num1 | num2) #Union
+print(num1 - num2) #Diference
+print(num1 ^ num2) #Symmetric Difference
+print(num1 & num2) #And
+
