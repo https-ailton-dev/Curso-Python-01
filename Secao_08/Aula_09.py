bec85223cf1d392dbe20f9834d9caaf52ffd8c42
Ailton Daniel <ailton.developer@gmail.com>
2024-02-19 19:43:54 -0300

    Input em uma lista

---
+++ Secao_08/Aula_09.py
@@ -0,0 +1,9 @@
+#Input em uma lista
+
+#criar uma lista de frutas a partir do input fornecido pelo usuário
+
+frutas_usuario = input('Digite o nome das frutas separados por virgula: ')
+
+frutas_lista = frutas_usuario.split(', ')
+
+print(frutas_lista)
