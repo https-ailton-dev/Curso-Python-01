b230cef44f2c22c9ea735958effc0f4464998dd6
Ailton Daniel <ailton.developer@gmail.com>
2024-02-19 22:05:50 -0300

    Conhecendo a Funcao Lambda

---
+++ Secao_08/Aula_19.py
@@ -0,0 +1,18 @@
+#Conhecendo a Função Lambda
+
+    # É uma função (pequena) sem nome
+    # Pode ter vários argumentos mas somente 1 expressão
+    # Muito utilizada dentro de outras funções
+    # Código mais 'clean'
+
+'''
+def somar(x):
+    return x + 10
+
+print(somar(2))
+
+'''
+
+somar10 = lambda x, y:  x + y + 10
+
+print(somar10(2, 4))
