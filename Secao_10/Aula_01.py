a4d8af12242b0824118114034e83b52709125139
Ailton Daniel <ailton.developer@gmail.com>
2024-02-20 16:01:32 -0300

    Conhecendo Classes

---
+++ Secao_10/Aula_01.py
@@ -0,0 +1,15 @@
+#Conhecendo Classes
+
+    # Utilizadas para criar Objetos (instances)
+    # Objetos são partes dentro de uma Class(instancias)
+    # Classes são utilizadas para agrupar dados e funções, podendo reutilizar
+    # ====
+    # Class: Frutas
+    # Objects: Abacate, Banana
+
+nome = 'Ailton'
+print(type(nome))
+#<class 'str'>
+
+nome_lower = nome.lower()
+print(nome_lower)
