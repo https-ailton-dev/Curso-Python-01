b00be57e86f14ff9d53c803d7d6fe60d4ac46290
Ailton Daniel <ailton.developer@gmail.com>
2024-02-20 17:15:28 -0300

    Criando seu primeiro Modulo | Importando um Modulo

---
+++ Secao_11/Aula_01.py
@@ -0,0 +1,8 @@
+#Criando seu primeiro Modulo | Importando um Modulo
+
+#import Funcao_Aula_01  #Vai importar todas as funções
+
+from Funcao_Aula_01 import somar, multi
+
+somar()
+multi()
---
+++ Secao_11/Funcao_Aula_01.py
@@ -0,0 +1,9 @@
+#Criando seu primeiro Modulo | Importando um Modulo
+def somar():
+    print('Esta Função vai somar valores')
+
+
+def multi():
+    print('Esta Função vai multiplicar valores')
+
+
