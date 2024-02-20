9ae0063445226e2a6bfca070a5901094ee054c8f
Ailton Daniel <ailton.developer@gmail.com>
2024-02-20 17:25:22 -0300

    Criando e Importando Packages

--- Secao_11/Aula_01.py
+++ Secao_11/Aula_01.py
@@ -1,8 +1,11 @@
-#Criando seu primeiro Modulo | Importando um Modulo
+#Criando seu primeiro Modulo | Importando um Modulo | Criando e Importando Packages
 
 #import Funcao_Aula_01  #Vai importar todas as funções
 
-from Funcao_Aula_01 import somar, multi
+from Funcao_Aula_01 import somar  #from Funcao_Aula_01 import somar, multi
+from Funcao_Aula_01 import multi
+from items.cadastro import cliente
 
 somar()
 multi()
+cliente()
