afc462ae82a8571da3d831046b5b32ea6bbc96dd
Ailton Daniel <ailton.developer@gmail.com>
2024-02-20 13:34:24 -0300

    Entendendo List Comprehension com numeros

---
+++ Secao_08/Aula_25.py
@@ -0,0 +1,14 @@
+#Entendendo List Comprehension com números
+
+valores = []
+
+'''
+for x in range(6):
+    valores.append(x*10)
+
+print(valores)
+
+'''
+valores2 = [x * 10 for x in range(6)]
+
+print(valores2)
