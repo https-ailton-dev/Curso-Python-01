ffdf90a11b0cfdedc6cb7904932982ca11d333d7
Ailton Daniel <ailton.developer@gmail.com>
2024-02-20 13:18:01 -0300

    Funcao Filter

---
+++ Secao_08/Aula_23.py
@@ -0,0 +1,13 @@
+#Função Filter
+
+    # Muito utilizado com listas
+    # Aplicar uma função a um Iterable, filtrando os items. (list, tuple, dic, etc.)
+
+valores = [10, 12, 34, 44, 57]
+
+'''def remover20(x):
+    return x > 20
+
+print(list(filter(remover20, valores)))'''
+
+print(list(filter(lambda x: x > 20, valores)))
