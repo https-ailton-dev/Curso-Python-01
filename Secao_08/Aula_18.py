a71b6def894168274ac648d34264676a118a6ced
Ailton Daniel <ailton.developer@gmail.com>
2024-02-19 21:54:08 -0300

    Visualizando Itens, Keys e Values

---
+++ Secao_08/Aula_18.py
@@ -0,0 +1,22 @@
+#Visualizando Itens, Keys e Values
+
+aluno = {
+    'nome': 'Ana', 
+    'idade': 16, 
+    'nota_final': 'A', 
+    'aprovação': True, 
+    'Matérias': ['Fisica', 'Matematica', 'Ingles']
+    }
+
+print(aluno)
+
+print(aluno.get('Matérias'))
+
+print(len(aluno))
+
+print(aluno.items())
+
+print(aluno.keys())
+
+print(aluno.values())
+
