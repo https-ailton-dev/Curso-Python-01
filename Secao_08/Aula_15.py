c399ad15c3f8b634e3d9a2ec651745cf67228439
Ailton Daniel <ailton.developer@gmail.com>
2024-02-19 21:36:14 -0300

    Introducao a Dicionarios

---
+++ Secao_08/Aula_15.py
@@ -0,0 +1,9 @@
+#Introdução a Dicionários
+
+    #Dicionários
+    #Utiliza index no formato de Keys e Values
+    #Aceita string, interger, float, boolean...
+
+aluno = {'nome': 'Ana', 'idade': 16, 'nota_final': 'A', 'aprovação': True}
+
+print(aluno['nome'])
