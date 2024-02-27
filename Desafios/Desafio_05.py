8c7fc3a6ad246bcafe1f564becbace25a844e170
Ailton Daniel <ailton.developer@gmail.com>
2024-02-26 22:09:11 -0300

    Desafio 05 - Listas

---
+++ Desafios/Desafio_05.py
@@ -0,0 +1,17 @@
+num1 = int(input('Digite o primeiro numero: '))
+num2 = int(input('Digite o segundo numero: '))
+
+# + (Adição ou sinal positivo)
+calculo1 = num1 + num2
+# - (Subtração ou sinal negativo)
+calculo2 = num1 - num2
+# * (Multiplicação)
+calculo3 = num1 * num2
+# / (Divisão)
+calculo4 = num1 / num2
+# ** (Exponenciação)
+calculo5 = num1 ** num2
+
+calculos = [calculo1, calculo2, calculo3, calculo4, calculo5]
+
+print(calculos)
