084da1de958ab639bf78b9da5c0869603c41ba7e
Ailton Daniel <ailton.developer@gmail.com>
2024-02-27 18:23:54 -0300

    Desafio 12 - Nested For Loop

---
+++ Desafios/Desafio_12.py
@@ -0,0 +1,8 @@
+frutas = ['Maçã', 'Morango', 'Uva']
+vegetais = ['Abobrinha', 'Berinjela', 'Pepino']
+
+
+#Outer loop
+for fruta in frutas:
+    for vegetal in vegetais:
+        print(fruta, vegetal)
