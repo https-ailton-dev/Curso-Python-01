d10b4b63984274cd4d77a07d470d00fe746cb82e
Ailton Daniel <ailton.developer@gmail.com>
2024-02-27 18:45:23 -0300

    Desafio 15 - Contador

---
+++ Desafios/Desafio_15.py
@@ -0,0 +1,9 @@
+frutas = ['Maçã', 'Morango', 'Maçã', 'Uva', 'Maçã']
+
+contador = 0
+
+for fruta in frutas:
+    if fruta == 'Maçã':
+        contador += 1
+
+print('Numero de Maçãs na Lista: ', contador)
