92a726dc19b6fc14d6ca7d4e9171d09e7f632ec8
Ailton Daniel <ailton.developer@gmail.com>
2024-02-28 09:24:21 -0300

    Desafio 25 - Funcao Soma

---
+++ Desafios/Desafio_25.py
@@ -0,0 +1,8 @@
+num1 = float(input('Por favor digite o primeiro numero: '))
+num2 = float(input('Por favor digite o segundo numero: '))
+
+def calculo_soma():
+    somar =  num1 + num2
+    return somar
+    
+print(calculo_soma())
