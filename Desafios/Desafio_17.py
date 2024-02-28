c45c692607e8a52cb50d77ef6de1331c965e1b6b
Ailton Daniel <ailton.developer@gmail.com>
2024-02-28 07:51:39 -0300

    Desafio 17 - Verificador de idade

---
+++ Desafios/Desafio_17.py
@@ -0,0 +1,10 @@
+idade = int(input('Digite a sua idade: '))
+
+if idade < 13:
+    print('Você é uma Criança')
+
+elif 13 <= idade <= 19:
+    print('Você é  um Adolescente')
+
+else:
+    print('Você é um Adulto')
