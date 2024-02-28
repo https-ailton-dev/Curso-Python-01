0c794ebf51fd86ddda7cd4346a4c648d95bc6faa
Ailton Daniel <ailton.developer@gmail.com>
2024-02-28 09:00:45 -0300

    Desafio 23 - Comparacao em Sets

---
+++ Desafios/Desafio_23.py
@@ -0,0 +1,7 @@
+list1 = ['João', 'Maria', 'Ana', 'Pedro', 'Sofia']
+list2 = ['Maria', 'Pedro', 'Laura', 'João', 'Mateus']
+
+setList1 = set(list1)
+setList2 = set(list2)
+
+print(setList1 & setList2) 
